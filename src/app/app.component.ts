45eff7ffc45e5bd2634dae3507cc5fbd309e6e79
Krishna Katakota <krk131@pitt.edu>
2024-07-12 21:28:51 -0400

    HTTPS url again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,8 +50,8 @@ export class AppComponent implements OnInit {
 		// const wsUrl = "ws://ec2-18-216-64-83.us-east-2.compute.amazonaws.com:8080/";
 
 		// HTTPS:
-		const wsUrl = "wss://ec2-18-216-64-83.us-east-2.compute.amazonaws.com:8081/";
-		
+		const wsUrl = "ws://ec2-18-216-64-83.us-east-2.compute.amazonaws.com:8081/";
+
 		this.wsService.connect(wsUrl);
 
 		this.subscription = this.subscribe();
